dfd4b4f97bd00c902ec571e9d03a12d05e7b44d9
Brooke Bryan <brooke@bajb.net>
2015-02-04 18:40:27 +0000

    Memcached to quit, rather than close

--- src/Cache/Memcache/MemcachedConnection.php
+++ src/Cache/Memcache/MemcachedConnection.php
@@ -3,6 +3,11 @@ namespace Packaged\Dal\Cache\Memcache;
 
 class MemcachedConnection extends MemcacheConnection
 {
+  /**
+   * @var \Memcached
+   */
+  protected $_connection;
+
   protected function _newConnection()
   {
     return new \Memcached($this->_config()->getItem('pool_name', null));
@@ -11,5 +16,20 @@ class MemcachedConnection extends MemcacheConnection
   protected function _addServer($server, $port, $persist, $weight, $timeout)
   {
     $this->_connection->addserver($server, $port, $persist);
+  }
+
+  /**
+   * Disconnect the open connection
+   *
+   * @return static
+   */
+  public function disconnect()
+  {
+    if($this->_connection !== null)
+    {
+      $this->_connection->quit();
+    }
+    $this->_connection = null;
+    return $this;
   }
 }

2c486b30a1173aa79c94320ff61e50a2a27a3b41
Brooke Bryan <brooke@bajb.net>
2015-02-05 16:02:26 +0000

    Counter data type

---
+++ src/DataTypes/Counter.php
@@ -0,0 +1,87 @@
+<?php
+namespace Packaged\Dal\DataTypes;
+
+class Counter implements IDataType
+{
+  public function __construct($original)
+  {
+    $this->_value = $original;
+    $this->_adjust = 0;
+    $this->_adjusted = false;
+  }
+
+  protected $_adjusted = false;
+  protected $_value = 0;
+  protected $_adjust = 0;
+
+  public function current()
+  {
+    return $this->_value;
+  }
+
+  public function setValue($value)
+  {
+    $this->_value = $value;
+    $this->_adjust = 0;
+    $this->_adjusted = true;
+  }
+
+  public function calculated()
+  {
+    return $this->_value + $this->_adjust;
+  }
+
+  public function increment($by = 1)
+  {
+    $this->_adjusted = true;
+    $this->_adjust += abs($by);
+    return $this;
+  }
+
+  public function decrement($by = 1)
+  {
+    $this->_adjusted = true;
+    $this->_adjust -= abs($by);
+    return $this;
+  }
+
+  public function isIncrement()
+  {
+    return $this->_adjust > 0;
+  }
+
+  public function isDecrement()
+  {
+    return $this->_adjust < 0;
+  }
+
+  public function isFixedValue()
+  {
+    return $this->_adjusted && $this->_adjust === 0;
+  }
+
+  public function hasChanged()
+  {
+    return $this->_adjusted;
+  }
+
+  public function getIncrement()
+  {
+    return $this->isIncrement() ? $this->_adjust : 0;
+  }
+
+  public function getDecrement()
+  {
+    return $this->isDecrement() ? $this->_adjust : 0;
+  }
+
+  public function getValue()
+  {
+    return $this->_value;
+  }
+
+  public function __toString()
+  {
+    return (string)$this->_value;
+  }
+}

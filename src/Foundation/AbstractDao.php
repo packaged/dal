0d3da6149725c74f9fbedc86e77c36ea83544dc9
cleeve <mike.cleeve@protected.net>
2023-05-23 16:22:02 +0100

    Fix type hint (#81)

--- src/Foundation/AbstractDao.php
+++ src/Foundation/AbstractDao.php
@@ -423,7 +423,7 @@ abstract class AbstractDao implements IDao
    * @return mixed data which can be serialized by <b>json_encode</b>,
    * which is a value of any type other than a resource.
    */
-  public function jsonSerialize()
+  public function jsonSerialize(): mixed
   {
     return Objects::propertyValues($this);
   }
